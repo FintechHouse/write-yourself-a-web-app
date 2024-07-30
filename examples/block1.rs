b01492aa11c8dda2f47c0061255f04ae26373df2
Matthias <matthias@endler.dev>
2024-07-30 10:26:50 +0200

    Simplify block 1

--- examples/block1.rs
+++ examples/block1.rs
@@ -1,17 +1,10 @@
 use axum::{routing::get, Router};
-use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     // Initialize the router
     let app = Router::new().route("/", get(hello_world));
-
-    // Set up the server address
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
-    println!("Server running on http://{}", addr);
-
-    // Start the server
+    println!("Server running on http://0.0.0.0:3000");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
