4c57d9d2c05f521300e75d30195cb3faba5dde95
palutz <5hge8s5h@duck.com>
2025-05-08 12:15:44 +0100

    nodb, cache in memory w/Hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use axum::extract::State;
 use axum::{
     routing::get,
     Router,
@@ -6,7 +7,7 @@ use axum::{
 
 use serde::{Deserialize, Serialize};
 
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Mutex};
 use std::collections::HashMap;
 
 #[derive(Deserialize)]
@@ -39,16 +40,17 @@ struct Hourly {
     temperature_2m: Vec<f64>,
 }
 
-// Write your code here.
+type MyCache = HashMap<String, LatLong>;
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let lcache = Arc::new(RwLock::new(
-            HashMap<String, LatLong>::new()));
+    let mut hmap : MyCache = HashMap::new();
+    let lcache = Arc::new(Mutex::new(hmap));
 
     let app = Router::new()
             .route("/", get(root))
-            .route("/weather", get(weather));
+            .route("/weather", get(weather))
             .with_state(lcache);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -59,22 +61,36 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn weather(Query(params): Query<WeatherQuery>) -> Result<String, String> {
-    let lat_long = get_latlong(&params.city).await.map_err(|e| e.to_string())?;
+async fn weather(params: Query<WeatherQuery>, State(lcache): State<Arc<Mutex<MyCache>>>) -> Result<String, String> {
+    let lat_long = get_latlong(lcache.clone(), &params.city).await.map_err(|e| e.to_string())?;
     let weather = fetch_weather(lat_long).await.map_err(|e| e.to_string())?;
     Ok(format!("Weather for {}: {:?}", params.city, weather))
 }
 
-async fn get_latlong(city: &str) -> Result<LatLong, Box<dyn std::error::Error>> {
+async fn get_latlong(lcache: Arc<Mutex<MyCache>>, city: &str) -> Result<LatLong, Box<dyn std::error::Error>> {
+    {
+        let lock = lcache.lock().unwrap();
+        match lock.get(city) {
+            Some(v) => return Ok(v.clone()),
+            _ => (),
+        }
+    };
+    // city not found in the cache: let's get it from the web
+
+    println!("City {city} not found in cache");
     let url = format!(
         "https://geocoding-api.open-meteo.com/v1/search?name={}&count=1&language=en&format=json",
         city
     );
     let response = reqwest::get(&url).await?.json::<GeoResponse>().await?;
-    response.results
-        .first()
-        .cloned()
-        .ok_or_else(|| "No results found".into())
+    match response.results.first() {
+        Some(v) => {
+            let mut lock = lcache.lock().unwrap();
+            lock.insert(city.to_string(), v.clone());
+            Ok(v.clone())
+        },
+        None => Err("No results found".into()),
+    }
 }
 
 async fn fetch_weather(lat_long: LatLong) -> Result<WeatherResponse, Box<dyn std::error::Error>> {
